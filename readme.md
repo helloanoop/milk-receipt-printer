4bc09cf19b134180501531cc72236f4e6b0210c5
Anoop M D <anoop.md1421@gmail.com>
2022-09-07 17:29:12 +0530

    feat: feat: initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# milk-receipt-printer
+Chippu's Dads Milk Receipt Printer
+
+## Develop
+```bash
+# install
+npm i
+
+# dev
+npm run dev
+```
---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <title>Milk Receipt Printer</title>
+        <meta name="Milk Receipt Printer" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="page page-home">
+        Hello World
+      </main>
+
+      <footer>
+      </footer>
+    </>
+  );
+};
