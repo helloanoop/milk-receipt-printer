fa6fb0dc6c67300c7ade239908fb19b0acae5b70
Anoop M D <anoop.md1421@gmail.com>
2022-09-10 20:45:37 +0530

    chore: fix lint issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }) {
         <meta charSet="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"/>
       </Head>
       <PrintProvider>
